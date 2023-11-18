da828d6e4bb94e988b5bcb2d03e17f9e78554521
Vaghela Darsh <77739833+darshvaghela@users.noreply.github.com>
2023-11-18 21:01:57 +0530

    Update Header.tsx

--- src/pages/dashboard/Header.tsx
+++ src/pages/dashboard/Header.tsx
@@ -20,7 +20,7 @@ const Header: FC = () => {
           style={{ justifyContent: "start", margin: "20px" }}
           alt="logo"
         />
-        <Title style={{ justifyContent: "center" }} level={2}>
+        <Title style={{ justifyContent: "center" }} level={3}>
           <b>Password Generator</b>
         </Title>
       </Content>
