9230a2360e44c4fecee658c625200c9304601945
Vaghela Darsh <77739833+darshvaghela@users.noreply.github.com>
2023-11-18 18:28:14 +0530

    Update Header.tsx

--- src/pages/dashboard/Header.tsx
+++ src/pages/dashboard/Header.tsx
@@ -7,7 +7,7 @@ const { Title } = Typography;
 
 const Header: FC = () => {
   return (
-    <Layout style={{ height: "10vh", backgroundColor: "#000000" }}>
+    <Layout style={{ height: "10vh", backgroundColor: "#FFFFFF" }}>
       <Content
         style={{
           display: "flex",
